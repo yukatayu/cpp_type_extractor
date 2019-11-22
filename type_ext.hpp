e1b9af3bc2472b6ff9b99b9402ba79ff85c96b3c
yukatayu <yukatayu.dev@gmail.com>
2019-11-22 14:48:59 +0900

    add: cpp_type_extractor

---
+++ type_ext.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <utility>
+
+namespace type_ext {
+
+// extract to
+template<template<class...> class T, template<class...> class U, class... Args>
+auto extract_to_impl(const U<Args...>&) -> T<Args...>;
+
+template<template<class...> class T, class U>
+using extract_to = decltype(extract_to_impl<T>(std::declval<U>()));
+
+// extract idx
+template<unsigned int i, class U, class... Args>
+struct extract_idx_getter{
+    using type = typename extract_idx_getter<i-1, Args...>::type;
+};
+
+template<class U, class... Args>
+struct extract_idx_getter<0, U, Args...>{ using type = U; };
+
+template<unsigned int i, template<class...> class U, class... Args>
+auto extract_idx_impl(const U<Args...>&) -> extract_idx_getter<i, Args...>;
+
+template<unsigned int i, class U>
+using extract_idx = typename decltype(extract_idx_impl<i>(std::declval<U>()))::type;
+
+}
---
+++ usage.cpp
@@ -0,0 +1,34 @@
+#include <memory>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+
+#include "type_ext.hpp"
+
+#include <boost/type_index.hpp>
+
+#define typeof(...) boost::typeindex::type_id_with_cvr<__VA_ARGS__>().pretty_name()
+
+#define assert_eq(x, ...) \
+    std::cout << "assert_equal(\n\t" << (#__VA_ARGS__) << ",\n\t" << (x) << "\n)\n" << std::endl; \
+    assert((x) == (__VA_ARGS__));
+
+// tester
+template<class A, class B, class C>
+struct TestStruct { };
+
+int main()
+{
+    using namespace std;
+
+    std::cout << "using test_t = TestStruct <int, char&, float&&>;\n" << std::endl;
+    using test_t = TestStruct <int, char&, float&&>;
+
+    assert_eq("std::tuple<int, char&, float&&>"s, typeof(type_ext::extract_to<std::tuple, test_t>));
+
+    assert_eq("int"s, typeof(type_ext::extract_idx<0, test_t>));
+    assert_eq("char&"s, typeof(type_ext::extract_idx<1, test_t>));
+    assert_eq("float&&"s, typeof(type_ext::extract_idx<2, test_t>));
+
+}
